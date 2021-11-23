60a238831a150a7f51f56126b357b9478ac8e4ed
GreenBear05 <med050595@gmail.com>
2021-11-23 17:08:30 +0300

    GreenBear

--- Lesson 17 LINQ/Program.cs
+++ Lesson 17 LINQ/Program.cs
@@ -10,36 +10,35 @@ using System.Text;
 namespace Lesson_17_LINQ {
     internal class Program {
         static void Main(string[] args) {
-
+            string Path = @"D:\path.txt";
             //var listcollection = new MyClasslist();
             //listcollection.workListCollection();
 
             var rand = new Random();
             
             var productsList = new List<Product>();
-            var workJson = new WorkJson<Product>();
-            
+            var workJson = new WorkJson<List<Product>>(Path);
+            var dictonory = new Dictionary<int,Product>();
 
             for (var i = 1;  i <= 10;  i++) {
+                
                 var product = new Product() { 
                     Name =  "Продукт"+ i,
                     Energy = rand.Next(10,50),
                 };
+                dictonory[i] = product;
                 productsList.Add(product);
             }
-
+            object a = productsList;
+            workJson.JsonWrite(productsList);
             var products = productsList.Where(item => item.Energy > 20);
 
-           workJson.JsonWrite(products);
-
             var productWrite = workJson.JsonRead();
+
             foreach (var item in productWrite) {
-                Console.WriteLine(item.Name + " " + item.Energy + "\n");
+                Console.WriteLine(item.ToString());
 
             }
-
-            
-            //Console.WriteLine(productWrite[0].Name);
             Console.ReadKey();
         }
     }
@@ -84,17 +83,18 @@ namespace Lesson_17_LINQ {
     public class Product {
         public string Name { get; set; }
         public int Energy { get; set; }
-
         //переопределение ToString()
-        //public override string ToString() {
-        //    return $"{Name} ({Energy})";
-        //}
+        public override string ToString() {
+            return $"[{Name} ({Energy})]";
+        }
     }
     public class WorkJson<T> {
-        string path = @"D:\path.txt";
-
+        string Path = @"D:\path.txt";
+        public WorkJson(string path) {
+            Path = path;
+        }
         
-        public void JsonWrite(IEnumerable<T> list) {
+        public void JsonWrite(T list) {
             
             var Options = new JsonSerializerOptions() {
                
@@ -103,30 +103,26 @@ namespace Lesson_17_LINQ {
             };
 
             var ser = JsonSerializer.Serialize(list,Options);
-            File.WriteAllText(path,ser);
+            File.WriteAllText(Path,ser);
                 
-                
-                
-           
-                
-           
         }
-        public IEnumerable<T> JsonRead() {
-            string path = @"D:\path.txt";
+        //public void JsonWrite(object list) {
+
+        //    var Options = new JsonSerializerOptions() {
+
+        //        WriteIndented = true,
+
+        //    };
+
+        //    var ser = JsonSerializer.Serialize(list, Options);
+        //    File.WriteAllText(Path, ser);
+
+        //}
+        public T JsonRead() {
             
-            var Options = new JsonSerializerOptions() {
-                WriteIndented = true,
-            };
-            var file = File.ReadAllText(path);
-            var ser = JsonSerializer.Deserialize<IEnumerable<T>>(file);
-            
-            
-
-
-
-
-
-
+           
+            var file = File.ReadAllText(Path);
+            var ser = JsonSerializer.Deserialize<T>(file);
 
             return ser;
 
